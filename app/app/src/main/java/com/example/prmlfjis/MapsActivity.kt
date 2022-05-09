3dcf898f8ea932c38b7bc1ff9faa762f1730f0d8
zrezke <jeretina.filip@gmail.com>
2022-05-09 22:25:39 +0200

    initialized android app with a polygon example

---
+++ app/app/src/main/java/com/example/prmlfjis/MapsActivity.kt
@@ -0,0 +1,67 @@
+package com.example.prmlfjis
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.example.prmlfjis.databinding.ActivityMapsBinding
+import com.google.android.gms.maps.model.PolygonOptions
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        // we can probably use Polygons to mark slovenian territory by municipality.
+        // when a user clicks on a polygon statistics about crime are shown
+        // when user zooms in more they can select a place, and an estimation of
+        // how dangerous the place is is shown?? perhaps
+        val rectOptions = PolygonOptions()
+            .add(
+                LatLng(-34.0, 151.0),
+                LatLng(-34.0, 160.0),
+                LatLng(-30.0, 160.0),
+            )
+            .strokeColor(Color.RED)
+            .fillColor(Color.BLUE)
+
+// Get back the mutable Polygon
+        val polygon = mMap.addPolygon(rectOptions)
+
+    }
+}
